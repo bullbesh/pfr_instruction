48fefd57f6012f923e2b086a8866e95217945347
bullbesh <babulehaha@gmail.com>
2021-08-18 20:20:40 +0300

    Change keyboard buttons logic

--- pfr_instruction/keyboard.py
+++ pfr_instruction/keyboard.py
@@ -1,33 +1,28 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
-UNDERSTAND_BUTTON = "Начнём!"
 BACK_BUTTON = "На главную"
 
-PENSION_BUTTON = "Я готовлюсь к пенсии"
-YEAR_PERIOD_BUTTON = "Остался год до пенсии"
+PENSION_BUTTON = "Готовлюсь к пенсии. С чего начать?"
+YEAR_PERIOD_BUTTON = "До пенсии остался год. Проверка документов."
 NEED_DOCUMENTS_BUTTON = "Список документов"
-SCHEDULE_BUTTON = "Часы приёма"
+SCHEDULE_BUTTON = "Часы приёма фронт-офиса"
 YEARLY_ATTENTION_BUTTON = "Важная информация"
-MONTH_ATTENTION_BUTTON = "Это полезно знать"
-MONTH_PERIOD_BUTTON = "До пенсии остался месяц"
+MONTH_ATTENTION_BUTTON = "Полезная информация"
+MONTH_PERIOD_BUTTON = "До пенсии остался месяц. Подача заявления."
 DOCUMENTS_MADE = "Да. И я сдавал документы на проверку заранее"
-WHEN_APPLY_BUTTON = "Когда подавать заявление?"
-HOW_APPLY_BUTTON = "Способы подачи заявления"
+WHEN_APPLY_BUTTON = "Когда и как подать заявление через Госуслуги?"
+PFR_APPEAL_BUTTON = "Обращение в клиентскую службу ПФР"
 ACROSS_GOSUSLUGI = "Через личный кабинет на сайте Госуслуги"
 ACROSS_CLIENT_SERVICE = "Через клиентскую службу ПФР"
 DOCUMENTS_NOT_MADE = "Да. Но я не сдавал документы на проверку заранее"
 
-EXPERIENCE_BUTTON = "Как можно узнать свой стаж?"
+EXPERIENCE_BUTTON = "Как узнать свой стаж на сайте Госуслуг?"
 GOSUSLUGI_BUTTON = "Госуслуги. Как зарегистрироваться?"
 MFC_REGISTRATION_BUTTON = "Через МФЦ"
 MOBILE_BANK_APP_REGISTRATION_BUTTON = "Через мобильное приложение банка"
 USEFUL_GOSUSLUGI_INFO_BUTTON = ""
 
-
-agree_markup = ReplyKeyboardMarkup(resize_keyboard=True).add(
-    KeyboardButton(UNDERSTAND_BUTTON)
-)
 
 main_markup = (
     ReplyKeyboardMarkup(resize_keyboard=True)
@@ -40,12 +35,13 @@ pension_period_markup = (
     ReplyKeyboardMarkup(resize_keyboard=True)
     .add(KeyboardButton(YEAR_PERIOD_BUTTON))
     .add(KeyboardButton(MONTH_PERIOD_BUTTON))
+    .add(KeyboardButton(NEED_DOCUMENTS_BUTTON))
 )
 
 pension_option_markup = (
     ReplyKeyboardMarkup(resize_keyboard=True)
     .add(KeyboardButton(WHEN_APPLY_BUTTON))
-    .add(KeyboardButton(HOW_APPLY_BUTTON))
+    .add(KeyboardButton(PFR_APPEAL_BUTTON))
     .add(KeyboardButton(MONTH_ATTENTION_BUTTON))
     .add(KeyboardButton(BACK_BUTTON))
 )
@@ -59,7 +55,6 @@ statement_pension_markup = (
 need_documents_markup = (
     ReplyKeyboardMarkup(resize_keyboard=True)
     .row(
-        KeyboardButton(NEED_DOCUMENTS_BUTTON),
         KeyboardButton(SCHEDULE_BUTTON),
         KeyboardButton(YEARLY_ATTENTION_BUTTON),
     )
@@ -70,7 +65,7 @@ when_and_how_apply = (
     ReplyKeyboardMarkup(resize_keyboard=True)
     .add(KeyboardButton(MONTH_PERIOD_BUTTON))
     .add(KeyboardButton(WHEN_APPLY_BUTTON))
-    .add(KeyboardButton(HOW_APPLY_BUTTON))
+    .add(KeyboardButton(PFR_APPEAL_BUTTON))
 )
 
 gosuslugi_registration_markup = (
